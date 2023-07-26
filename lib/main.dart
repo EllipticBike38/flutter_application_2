4dd21acfa029b63ff5f643fd3c8cd24594d42551
Andrea Mazzini <andrea.mazzini00@gmail.com>
2023-07-26 23:41:44 +0200

    cleaning imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
